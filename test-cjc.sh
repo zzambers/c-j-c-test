6a32378539ef7aad0dfe0e0dd9993b8d75331d9b
Zdenek Zambersky <zzambers@redhat.com>
2018-08-14 17:06:34 +0200

    test-cjc.sh: Fixed jdkDirName for ibm javas

--- test-cjc.sh
+++ test-cjc.sh
@@ -247,6 +247,12 @@ getJdkHomeDir() (
 	elif printf '%s\n' "${jdkName}" | grep -q '^java-1.8.0-oracle' \
 	&& printf '%s\n' "${newJdkRelease:-}" | grep -q '\.el6' ; then
 		jdkDirName="${name}-${version}.${architecture}"
+	elif printf '%s\n' "${jdkName}" | grep -q '^java-1.7.1-ibm' \
+	&& printf '%s\n' "${newJdkRelease:-}" | grep -q '\.el6' ; then
+		jdkDirName="${name}-${version}.${architecture}"
+	elif printf '%s\n' "${jdkName}" | grep -q '^java-1.8.0-ibm' \
+	&& printf '%s\n' "${newJdkRelease:-}" | grep -q '\.el6' ; then
+		jdkDirName="${name}-${version}.${architecture}"
 	else
 		jdkDirName="${name}-${version}-${release}.${architecture}"
 	fi
